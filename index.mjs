0f2fee9d461bbacd138a8a4d2dfa0e68eb14b30a
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-09-17 21:23:21 -0300

    updating response

--- index.mjs
+++ index.mjs
@@ -6,5 +6,8 @@ export const handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(event),
+    Headers: {
+      "Content-Type": "application/json"
+    }
   };
 };

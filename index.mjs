d6c5c43467d413062726cb91e256e4e6e56fd94a
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-09-17 21:23:25 -0300

    updating response

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { log } from "./log.mjs";
 
 export const handler = async (event) => {  
-  log("Hello from handler after github action" + JSON.stringify(event));
+  log("logs: " + JSON.stringify(event));
 
   return {
     statusCode: 200,

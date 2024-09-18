afedc941252c009a2042dfb62211b5bf02493b25
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-09-17 21:41:36 -0300

    adding test with variables enviroment

--- index.mjs
+++ index.mjs
@@ -1,13 +1,15 @@
 import { log } from "./log.mjs";
 
 export const handler = async (event) => {  
-  log("logs: " + JSON.stringify(event));
+  console.log("logs: " + JSON.stringify(event));
+  console.log(process.env.my_key);
+  
 
   return {
     statusCode: 200,
     body: JSON.stringify(event),
     Headers: {
       "Content-Type": "application/json"
-    }
+    },
   };
 };

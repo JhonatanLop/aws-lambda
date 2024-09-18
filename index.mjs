fc871f40cfee566a87b92b0205ae6058edab4bee
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-09-17 22:16:54 -0300

    adding test with s3 put size

--- index.mjs
+++ index.mjs
@@ -1,14 +1,21 @@
 import { log } from "./log.mjs";
+import { S3 } from '@aws-sdk/client-s3';
 
-export const handler = async (event) => {  
-  console.log("logs: " + JSON.stringify(event));
-  
+const s3Client = new S3({ region: 'us-east-2' });
+export const handler = async (event, _) => {  
+  const record = event.Record[0];
+  const Bucket = record.s3.bucket.name;  
+  const Key  = record.s3.object.key;
+  const getObjectResult = s3Client.getObject({ 
+    Bucket, Key 
+  });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event),
-    Headers: {
-      "Content-Type": "application/json"
-    },
-  };
+  const megaByte = 1024 * 1024;
+
+  if (getObjectResult.contentLength > 1 * megaByte) {
+    log("objeto muito grande");
+    return ("objeto muito grande");
+  }
+
+  return "Objeto de tamanho OK";
 };
